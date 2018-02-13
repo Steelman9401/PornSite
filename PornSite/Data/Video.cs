ab0c370f4303e27ba1b188d4358e93989b9334c8
Dominik Brych <dominik.brych@seznam.cz>
2018-02-13 20:31:02 +0100

    fix Description

--- PornSite/Data/Video.cs
+++ PornSite/Data/Video.cs
@@ -10,7 +10,7 @@ namespace PornSite.Data
         public int Id { get; set; }
         public string Title { get; set; }
         public string Url { get; set; }
-        public string Desc { get; set; }
+        public string Description { get; set; }
         public string Img { get; set; }
         public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
     }
