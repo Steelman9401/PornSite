8fb1ba8a4a7b9070a7fdc2c9e5b04c960ca5add5
Dominik Brych <dominik.brych@seznam.cz>
2018-02-23 16:31:55 +0100

    init

---
+++ PornSite/obj/Release/Package/PackageTmp/Content/scripts.js
@@ -0,0 +1,17 @@
+﻿function playVideo(e) {
+    e.play();
+}
+function stopVideo(e) {
+    e.load();
+    e.pause();
+}
+function pressButton(e) {
+    e.children[0].click();
+}
+
+$(document).ready(function () {
+    $("#menu-icon").click(function () {
+        $("#form").slideToggle(200);
+        $("#menu").slideToggle(200);
+    });
+});
